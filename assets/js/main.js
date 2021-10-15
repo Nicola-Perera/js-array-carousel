b44bc94af8d7f290f795047288809f403adbfc6a
Nicola-Perera <nicola.perera96@mail.com>
2021-10-15 14:37:42 +0200

    add steps in order to write the program in javascript

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+/* 
+    Programma per scorrere tramite 2 pulsanti (Up & Down) 
+    fra le miniature di 5 paesaggi differenti.
+    Nello stato di una miniatura selezionata, 
+    il paesaggio viene mostrato nella schermata principale
+    e la miniatura è in evidenza rispetto a quelle non selzionate.
+*/ 
+
+// collego la schermata principale e le miniature ciascuno ad una variabile dedicata;
+
+// inserisco ogni immagine/titolo/testo in una lista
+
+// creo delle variabili ciascuna contente un indice collegato ad un elemento delle liste
+
+// aggiungo ai pulsanti la funzione di aumentare/diminuire di una unità l'indice delle variabili
+
+// tramite un if controllo quale indice si trova dentro la variabile
+// in base al valore dell'indice aggiungo lo stato "selected" alla corrispondente variabile
+// lo stato selected aggiunge le classi agli elementi html 
+
+/*
+ BONUS: tramite un if gestisco gli estremi 0 e 4 della lista miniature:
+        0 -1 = 4
+        4 + 1 = 0
+ */
